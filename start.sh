cb5db6cfb779300507e0ddb5224a632722d3bd91
krumware <colin@krum.io>
2020-09-14 20:36:50 -0400

    add make migrations

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-./wait-for-it.sh -h $DB_HOST -p $DB_PORT -t 20 -- python manage.py migrate
+./wait-for-it.sh -h $DB_HOST -p $DB_PORT -t 20 -- python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8100
 # gunicorn -c /code/gunicorn.conf 'config.wsgi'
