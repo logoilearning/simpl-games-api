abf419129fd38b416066016b9bac02808388e415
Colin Griffin <colin@krum.io>
2020-09-15 11:42:51 -0400

    add create_simpl_user

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ./wait-for-it.sh -h $DB_HOST -p $DB_PORT -t 20 -- python manage.py migrate
+python manage.py create_simpl_user
 python manage.py collectstatic --noinput
-gunicorn -c /code/gunicorn.conf 'config.wsgi'
+python manage.py runserver 0.0.0.0:8100
+# gunicorn -c /code/gunicorn.conf 'config.wsgi'
